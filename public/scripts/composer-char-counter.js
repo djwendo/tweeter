3ff5625baafdc846d2c2f64e41c082a708c83cd7
Wendy Braun <wendocalrissian@gmail.com>
2018-03-27 21:03:23 +0000

    Character counter changes color when tweet is too long

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  $('textarea').on('keyup', function() {
-    var count = this.textLength;
+  $('textarea').on('input', function() {
+    var count = $(this).val().length;
     var charCounter = $(this).parent().find(".counter");
     charCounter.text(140 - count);
+    if (count > 140) {
+      $(".counter").css("color", "#ff0000");
+    } else {
+      $(".counter").css("color", "#244751");
+    }
   });
 });
